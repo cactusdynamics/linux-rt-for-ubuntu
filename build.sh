1fa9bc8b4c6e1391fd418412c61151691afce5da
Shuhao Wu <shuhao@shuhaowu.com>
2022-11-14 20:45:14 -0500

    Updated kernel

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -xe
 
 cd /workspace
-git clone -b Ubuntu-5.15.0-53.59 --depth=1 https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy linux-src
+git clone -b Ubuntu-5.15.0-54.60 --depth=1 https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/jammy linux-src
 mkdir preempt-rt-patches
 cd preempt-rt-patches
-wget https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.15/older/patches-5.15.65-rt49.tar.gz
+wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.15/older/patches-5.15.73-rt52.tar.gz
 tar xvf patches*
 cd ../linux-src
 for p in ../preempt-rt-patches/patches/*.patch; do
@@ -25,7 +25,7 @@ scripts/kconfig/merge_config.sh \
 
 # Do not generate dbg package as that is very time consuming and generates a
 # large debian package.
-scripts/config --disable DEBUG_INFO
+# scripts/config --disable DEBUG_INFO
 
 time make bindeb-pkg -j$(nproc)
 
